fb91edba90d2b1c507f26e8c95b0df3ac8f769e5
Rajasekhar K <rajasekhar@user.com>
2026-04-29 21:16:58 +0530

    fix: switch CI to bash shell for glob support, bump v0.1.13

--- pap_ai_era/__init__.py
+++ pap_ai_era/__init__.py
@@ -3,7 +3,7 @@ PapAiEra: Pulp and Paper Industry Optimization Library
 Following Best Available Techniques (BAT) BREF 2015 standards.
 """
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 from . import pulping
 from . import papermaking
 from . import sustainability
